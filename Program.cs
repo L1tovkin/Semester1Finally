f876b4ad79913fed1b8172b2df476a132754802d
Anton <l1tovkin@mail.ru>
2022-11-16 20:29:59 +0400

    Added code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine();
+string[] firstArray = new string[4] { "hello", "2", "world", ":-)" };
+string[] secondArray = new string[firstArray.Length];
+void secondArray2(string[] firstArray, string[] secondArray)
+
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+PrintArray(firstArray);
+Console.WriteLine();
+Console.WriteLine("Ваш массив: ");
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+secondArray2(firstArray, secondArray);
+PrintArray(secondArray);
+Console.WriteLine();
